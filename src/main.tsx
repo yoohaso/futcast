3816977de9571333e8ea03dcfc9bc1349da7a0a0
yoohaso <solhanyun@gmail.com>
2025-01-07 23:38:19 +0900

    chore: add global styles using emotion's global component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { Global, css } from '@emotion/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global
+      styles={css`
+        body {
+          width: 100%;
+          height: 100vh;
+          margin: 0;
+          background-color: #ffffff;
+          font-size: 16px;
+        }
+
+        h1,
+        h2,
+        p {
+          margin: 0;
+        }
+      `}
+    />
     <App />
   </StrictMode>
 );
