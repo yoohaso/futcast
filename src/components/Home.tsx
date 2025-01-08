36f09cee6a6a7c051f901d7c17fb594aa1b929f4
yoohaso <solhanyun@gmail.com>
2025-01-08 14:28:14 +0900

    refactor: seperate components into individual files

---
+++ src/components/Home.tsx
@@ -0,0 +1,25 @@
+import { css } from '@emotion/react';
+import Board from './Board';
+
+function Home() {
+  return (
+    <div
+      css={css({
+        maxWidth: '462px',
+        width: '100%',
+        backgroundColor: '#070c25',
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
+      })}
+    >
+      <h1 css={css({ color: '#ffffff', padding: '15px', fontSize: '1.5rem' })}>풋캐스트</h1>
+      <main css={css({ flex: 1 })}>
+        <Board />
+      </main>
+      <footer css={css({ height: '8vh' })}></footer>
+    </div>
+  );
+}
+
+export default Home;
