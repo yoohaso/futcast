296630d91e87431ccf72fc434a94bfbfdc1995e4
yoohaso <solhanyun@gmail.com>
2025-01-11 01:19:19 +0900

    feat: add fetchMatches to retrieve match data from supabase

---
+++ src/api/types.ts
@@ -0,0 +1,20 @@
+type Base = {
+  createdAt: string;
+  updatedAt: string | null;
+  deletedAt: string | null;
+};
+
+type Field = {
+  id: number;
+  address: string;
+  weatherLink: string;
+  gridX: number;
+  gridY: number;
+} & Base;
+
+export type Match = {
+  id: number;
+  schedule: string;
+  plabfootballLink: string | null;
+  field: Field;
+} & Base;
