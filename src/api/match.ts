f8683fce3f821af7997a106f3493fdc4c51dd5e9
yoohaso <solhanyun@gmail.com>
2025-01-11 01:19:19 +0900

    feat: add fetchMatches to retrieve match data from supabase

---
+++ src/api/match.ts
@@ -0,0 +1,17 @@
+import { createClient, PostgrestSingleResponse } from '@supabase/supabase-js';
+import { Match } from './types';
+
+type FetchMatchesResponse = PostgrestSingleResponse<Match[]>;
+
+const { VITE_SUPABASE_URL, VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY } = import.meta.env;
+const supabase = createClient(VITE_SUPABASE_URL, VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+export async function fetchMatches() {
+  const response: FetchMatchesResponse = await supabase.rpc('get_matches_by_schedule');
+
+  if (response.error) {
+    throw new Error(response.error.message);
+  }
+
+  return response.data;
+}
