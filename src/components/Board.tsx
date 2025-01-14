193f552a60c550c109c919c39f5e32934f16f0f8
yoohaso <solhanyun@gmail.com>
2025-01-15 01:14:13 +0900

    feat: display temperature in DayCard component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,9 +2,23 @@ import { css } from '@emotion/react';
 import CurrentDayArea from './CurrentDayArea';
 import OtherDaysArea from './OtherDaysArea';
 import useMatches from '../hooks/useMatches';
+import { useMemo } from 'react';
+import useWeather from '../hooks/useWeather';
 
 function Board() {
   const [matches, isLoading] = useMatches();
+
+  const locations = useMemo(
+    () =>
+      matches.map(match => ({
+        timestamp: match.schedule,
+        gridX: match.field.gridX,
+        gridY: match.field.gridY,
+      })),
+    [matches]
+  );
+
+  const [weather] = useWeather({ locations });
 
   return (
     <div
@@ -18,9 +32,12 @@ function Board() {
     >
       {!isLoading && (
         <>
-          <CurrentDayArea match={matches[0]} />
+          <CurrentDayArea match={matches[0]} weather={weather[0] ? weather[0] : null} />
           <hr css={css({ width: '100%', border: '1px solid white' })} />
-          <OtherDaysArea matches={matches.slice(1, matches.length)} />
+          <OtherDaysArea
+            matches={matches.slice(1, matches.length)}
+            weather={weather.slice(1, weather.length) ? weather.slice(1, weather.length) : null}
+          />
         </>
       )}
     </div>
--- src/components/CurrentDayArea.tsx
+++ src/components/CurrentDayArea.tsx
@@ -1,13 +1,14 @@
 import { css } from '@emotion/react';
 import DayCard from './DayCard';
-import { Match } from '../api/types';
+import { Match, Weather } from '../api/types';
 import { formatDate, formatTime } from '../utils';
 
 type CurrentDayAreaProps = {
   match: Match;
+  weather: Weather | null;
 };
 
-function CurrentDayArea({ match }: CurrentDayAreaProps) {
+function CurrentDayArea({ match, weather }: CurrentDayAreaProps) {
   return (
     <div
       css={css({
@@ -16,7 +17,12 @@ function CurrentDayArea({ match }: CurrentDayAreaProps) {
       })}
     >
       <h2 css={css({ padding: '10px 0', fontSize: '1rem', color: '#ffffff' })}>오늘</h2>
-      <DayCard date={formatDate(match.schedule)} fieldName={match.field.name} startTime={formatTime(match.schedule)} />
+      <DayCard
+        date={formatDate(match.schedule)}
+        fieldName={match.field.name}
+        startTime={formatTime(match.schedule)}
+        temp={weather?.temperature.value}
+      />
     </div>
   );
 }
