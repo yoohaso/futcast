06457303b6e23f2dfcfc38fe9aba698f44620425
yoohaso <solhanyun@gmail.com>
2025-01-16 15:58:37 +0900

    style: modify location of h2 tag in Board component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -29,30 +29,22 @@ function Board() {
         width: 'inherit',
         display: 'flex',
         flexDirection: 'column',
-        gap: '2vh',
         padding: '0 15px 2vh 15px',
       })}
     >
+      <h2 css={css({ padding: '10px 0', fontSize: '1rem', color: '#ffffff' })}>다가오는 매치</h2>
       {!isLoading && (
         <>
-          <div
-            css={css({
-              width: 'inherit',
-              paddingBottom: '10px',
-            })}
-          >
-            <h2 css={css({ padding: '10px 0', fontSize: '1rem', color: '#ffffff' })}>오늘</h2>
-            <DayCard
-              date={formatDate(upcomingMatch!.schedule)}
-              fieldName={upcomingMatch!.field.name}
-              startTime={formatTime(upcomingMatch!.schedule)}
-              temp={upcomitMatchWeather?.temperature.value}
-              skyCondition={upcomitMatchWeather?.skyCondition.value}
-              precipitationType={upcomitMatchWeather?.precipitationType.value}
-              precipitationProbability={upcomitMatchWeather?.precipitationProbability.value}
-            />
-          </div>
-          <hr css={css({ width: '100%', border: '1px solid white' })} />
+          <DayCard
+            date={formatDate(upcomingMatch!.schedule)}
+            fieldName={upcomingMatch!.field.name}
+            startTime={formatTime(upcomingMatch!.schedule)}
+            temp={upcomitMatchWeather?.temperature.value}
+            skyCondition={upcomitMatchWeather?.skyCondition.value}
+            precipitationType={upcomitMatchWeather?.precipitationType.value}
+            precipitationProbability={upcomitMatchWeather?.precipitationProbability.value}
+          />
+          <hr css={css({ width: '100%', margin: '15px 0', border: '1px solid white' })} />
           <div
             css={css({
               width: 'inherit',
