9c191bb755ba9520f881dbf3e1f2007fd65c8c5c
Hansol Yun <solhanyun@gmail.com>
2025-01-18 21:45:09 +0900

    feat: add static UI for modal content

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -27,26 +27,36 @@ function Modal({ onClose, children }: ModalProps) {
           left: '50%',
           transform: 'translate(-50%, -50%)',
           width: '90%',
-          height: '35%',
+          height: 'auto',
           backgroundColor: 'white',
           boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
           borderRadius: '8px',
           zIndex: '1001',
           overflow: 'hidden',
           maxWidth: '380px',
-          maxHeight: '400px',
-          padding: '10px 20px 20px 20px',
           display: 'flex',
           flexDirection: 'column',
           boxSizing: 'border-box',
         })}
       >
-        <div css={css({ alignSelf: 'flex-end' })}>
-          <button type="button" onClick={onClose} css={css({ border: 'none', backgroundColor: 'white' })}>
+        <div>
+          <button
+            type="button"
+            onClick={onClose}
+            css={css({
+              position: 'fixed',
+              border: 'none',
+              color: '#ffffff',
+              backgroundColor: 'transparent',
+              top: '10px',
+              right: '10px',
+              fontSize: '15px',
+            })}
+          >
             X
           </button>
         </div>
-        <div css={css({ height: '100%', marginTop: '50px' })}>{children}</div>
+        <div>{children}</div>
       </div>
     </div>,
     document.body
