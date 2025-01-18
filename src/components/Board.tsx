39386a22e8af8bdd6650946c1bfbcce9bdce0208
Hansol Yun <solhanyun@gmail.com>
2025-01-18 21:45:09 +0900

    feat: add static UI for modal content

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,6 +5,7 @@ import useWeather from '../hooks/useWeather';
 import DayCard from './DayCard';
 import { formatDate, formatTime } from '../utils';
 import Modal from './Modal';
+import { Clock, CloudRain, Droplets, ExternalLink, MapPin, ThermometerSun } from 'lucide-react';
 
 type ClickableProps = {
   onClick: () => void;
@@ -95,22 +96,118 @@ function Board() {
       </div>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
-          <div css={css({ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' })}>
-            <div css={css({ display: 'flex', justifyContent: 'space-between' })}>
-              <div css={css({ display: 'flex', flexDirection: 'column', gap: '2px' })}>
-                <span css={css({ display: 'block', fontWeight: 'bold', fontSize: '20px' })}>신도림 구장</span>
-                <span css={css({ display: 'block', fontSize: '15px' })}>10시 시작</span>
-                <span css={css({ display: 'block', fontSize: '15px' })}>서울시 구로구 신도림동</span>
+          <div css={css({ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' })}>
+            <div
+              css={css({
+                display: 'flex',
+                height: '100px',
+                backgroundColor: '#070c25',
+                padding: '15px 10px 20px 15px',
+                justifyContent: 'space-between',
+                alignItems: 'flex-end',
+              })}
+            >
+              <div>
+                <span css={css({ display: 'block', fontWeight: 'bold', fontSize: '20px', color: '#ffffff' })}>
+                  신도림 구장
+                </span>
+                <div css={css({ display: 'flex', alignItems: 'center', gap: '5px', marginTop: '5px' })}>
+                  <Clock color="#ffffff" size={15} />
+                  <span css={css({ display: 'block', fontSize: '15px', color: '#ffffff' })}>10시 시작</span>
+                </div>
               </div>
-              <div css={css({ display: 'flex', flexDirection: 'column', gap: '2px' })}>
-                <span css={css({ display: 'block', fontSize: '15px' })}>20도</span>
-                <span css={css({ display: 'block', fontSize: '15px' })}>강수확률 30%</span>
-                <span css={css({ display: 'block', fontSize: '15px' })}>강수량 1mm</span>
-              </div>
+              <img
+                css={css({ width: '70px', height: '70px' })}
+                src="https://openweathermap.org/img/wn/10d@2x.png"
+                alt="weather_icon"
+              />
             </div>
-            <div css={css({ display: 'flex', justifyContent: 'space-between' })}>
-              <a css={css({ fontSize: '12px' })}>플랩풋볼</a>
-              <a css={css({ fontSize: '12px' })}>기상청</a>
+            <div
+              css={css({
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                padding: '20px',
+                backgroundColor: '#ffffff',
+              })}
+            >
+              <div
+                css={css({
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                })}
+              >
+                <div css={css({ display: 'flex', alignItems: 'center', gap: '5px', marginBottom: '15px' })}>
+                  <MapPin size={14} />
+                  <span css={css({ display: 'block', fontSize: '14px' })}>서울시 구로구 신도림동</span>
+                </div>
+                <div css={css({ display: 'flex', justifyContent: 'space-between', marginBottom: '15px' })}>
+                  <div
+                    css={css({
+                      display: 'flex',
+                      boxSizing: 'border-box',
+                      width: '45%',
+                      height: '70px',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      padding: '10px',
+                      borderRadius: '8px',
+                      gap: '8px',
+                      backgroundColor: '#f5f5f5',
+                    })}
+                  >
+                    <div css={css({ display: 'flex', alignItems: 'center', gap: '5px' })}>
+                      <ThermometerSun size={14} />
+                      <span css={css({ display: 'block', fontSize: '12px' })}>기온</span>
+                    </div>
+                    <span css={css({ display: 'block', fontSize: '17px', fontWeight: 'bold' })}>20도</span>
+                  </div>
+                  <div
+                    css={css({
+                      display: 'flex',
+                      boxSizing: 'border-box',
+                      width: '45%',
+                      height: '70px',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      padding: '10px',
+                      borderRadius: '8px',
+                      gap: '8px',
+                      backgroundColor: '#f5f5f5',
+                    })}
+                  >
+                    <div css={css({ display: 'flex', alignItems: 'center', gap: '5px' })}>
+                      <CloudRain size={14} />
+                      <span css={css({ display: 'block', fontSize: '12px' })}>강수확률</span>
+                    </div>
+                    <span css={css({ display: 'block', fontSize: '17px', fontWeight: 'bold' })}>30%</span>
+                  </div>
+                </div>
+                <div css={css({ display: 'flex', alignItems: 'center', gap: '5px' })}>
+                  <Droplets size={14} />
+                  <span css={css({ display: 'block', fontSize: '14px' })}>강수량 1mm</span>
+                </div>
+              </div>
+              <hr
+                css={css({
+                  width: '100%',
+                  border: 'none',
+                  height: '0.5px',
+                  backgroundColor: 'black',
+                  margin: '20px 0',
+                })}
+              />
+              <div css={css({ display: 'flex', justifyContent: 'space-between' })}>
+                <a css={css({ display: 'flex', gap: '5px', alignItems: 'center', fontSize: '14px' })}>
+                  플랩풋볼
+                  <ExternalLink size={13} />
+                </a>
+                <a css={css({ display: 'flex', gap: '5px', alignItems: 'center', fontSize: '14px' })}>
+                  상세날씨
+                  <ExternalLink size={13} />
+                </a>
+              </div>
             </div>
           </div>
         </Modal>
