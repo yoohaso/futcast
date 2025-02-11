b206ec93e7a7d485989376710d6852680f6f08d3
yoohaso <solhanyun@gmail.com>
2025-02-11 17:39:07 +0900

    fix: handle empty matches

--- src/components/Board/StadiumGames.tsx
+++ src/components/Board/StadiumGames.tsx
@@ -2,13 +2,35 @@ import { Suspense } from 'react';
 import useMatches from '../../hooks/useMatches';
 import { StadiumGamesWithWeatherSkeleton } from './StadiumGamesWithWeatherSkeleton';
 import { StadiumGamesWithWeather } from './StadiumGamesWithWeather';
+import { css } from '@emotion/react';
+
+function EmptyGame() {
+  return (
+    <div
+      css={css({
+        width: 'inherit',
+        height: '25vh',
+        maxHeight: '166px',
+        backgroundColor: '#fffff0',
+        borderRadius: '10px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '0 20px',
+        fontWeight: 'bold',
+      })}
+    >
+      ⚽️ 경기가 없습니다 ⚽️
+    </div>
+  );
+}
 
 export function StadiumGames() {
   const matches = useMatches();
 
   return (
     <Suspense fallback={<StadiumGamesWithWeatherSkeleton matches={matches} />}>
-      <StadiumGamesWithWeather matches={matches} />
+      {matches.length !== 0 ? <StadiumGamesWithWeather matches={matches} /> : <EmptyGame />}
     </Suspense>
   );
 }
